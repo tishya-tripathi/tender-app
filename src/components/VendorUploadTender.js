97f2702e1b1ffcff37a6ccdf45e84f8390ec0fe2
Tishya Tripathi <tishyatripathi@outlook.com>
2022-09-05 08:57:43 +0530

    Brrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- src/components/VendorUploadTender.js
+++ src/components/VendorUploadTender.js
@@ -37,9 +37,9 @@ const VendorUploadTender = () => {
         };
 
         // Comment these 3 statements if all available tenders are to be shown under Vendor Upload
-        // const sDate = new Date(obj.startDate);
-        // const eDate = new Date(obj.endDate);
-        // if (currentDate > sDate && currentDate < eDate)
+        const sDate = new Date(obj.startDate);
+        const eDate = new Date(obj.endDate);
+        if (currentDate > sDate && currentDate < eDate)
           temp_data.push(obj);
       }
 
